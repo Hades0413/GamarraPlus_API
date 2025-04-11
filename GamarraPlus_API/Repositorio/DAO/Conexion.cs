cd9ccbd72e172f93c2819f31df1ee95bac1d59a6
Hades0413 <migvel.gomero.1901@hotmail.com>
2025-04-11 17:05:30 -0500

    Update connection string and project paths for environment consistency

--- GamarraPlus_API/Repositorio/DAO/Conexion.cs
+++ GamarraPlus_API/Repositorio/DAO/Conexion.cs
@@ -7,7 +7,7 @@
         public Conexion()
         {
             var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
-            cadenaSQL = builder.GetSection("ConnectionStrings:sql").Value;
+            cadenaSQL = builder.GetSection("ConnectionStrings:sqlConUser").Value;
         }
 
         public string getCadenaSQL()
